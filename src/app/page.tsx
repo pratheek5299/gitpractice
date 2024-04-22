2b2ba108b4f049a4939ae7f38b398a1b0560bc37
Pratheek <saipratheekreddykasarla@gmail.com>
2024-04-22 18:08:46 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+// this is comment
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
